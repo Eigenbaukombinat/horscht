e57dd806606770ced54245ca11d10b7e3a8699b5
der_kleine_nilo <admin@eigenbaukombinat.de>
2019-06-02 11:57:34 +0200

    speak funktion

---
+++ modules/speak.py
@@ -0,0 +1,15 @@
+import datetime
+import requests
+import logging
+
+
+
+def talk(event, message, bot, args):
+    """Lässt Bernd sprechen."""
+    sender = event['sender']
+    atuser, server = sender.split(':')
+    user = atuser[1:]
+    text = '{}#{}'.format(user, ' '.join(args))
+    bot.mqtt_client.publish('space/bernd/speak/chat', text)
+
+CMDS = { '!talk': talk }
