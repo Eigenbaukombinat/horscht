eb30be8c862b6e14db81431882e490c3e3a300c7
nilo <nilo@team-tfm.com>
2024-11-12 21:27:34 +0100

    adapt code to doc

--- main.py
+++ main.py
@@ -211,8 +211,8 @@ class Bot(object):
 
     def handle_message(self, event, message):
         command_found = False
-        for msghandler in MSGHANDLERS:
-            msghandler(event, message)
+        for msghandler, module_name in MSGHANDLERS:
+            msghandler(event, message, self, MODULE_CONFIG[module_name])
         for command in COMMANDS:
             match = re.search(command, message, flags=re.IGNORECASE)
             if match and (match.start() == 0 or
@@ -392,7 +392,7 @@ def main():
             CRON_REGISTRY.append((config[module_name]["secs"], mod.CRON, module_name))
         if hasattr(mod, 'MSGHANDLERS'):
             for func in mod.MSGHANDLERS:
-                MSGHANDLERS.append(func)
+                MSGHANDLERS.append((func, module_name))
     COMMANDS.extend(list(COMMAND_REGISTRY.keys()))
 
 
