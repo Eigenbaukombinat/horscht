744917cc03410526a5412b9ab10a5c05a721aead
¯\_(ツ)_/¯ <>
2025-03-06 22:35:22 +0100

    spacebot NG 🪄

--- modules/spacebot.py
+++ modules/spacebot.py
@@ -1,10 +1,15 @@
 import datetime
 import requests
 import logging
+import os
+import shutil
+import tempfile
+import json
+
 
 shlog = logging.getLogger('shlog')
 shlog.setLevel(logging.DEBUG)
-shlog_fh = logging.FileHandler('/home/spaceapi/spacehistory.log')
+shlog_fh = logging.FileHandler('spacehistory.log')
 shlog_formatter = logging.Formatter('%(asctime)s - %(message)s')
 shlog_fh.setFormatter(shlog_formatter)
 shlog.addHandler(shlog_fh)
@@ -12,6 +17,8 @@ shlog.addHandler(shlog_fh)
 SPACESTATUS_URL = 'https://eigenbaukombinat.de/status/status.json'
 SPACEOPEN_URL = 'https://eigenbaukombinat.de/status/openuntil.json'
 CURRENT_STATUS = 'zu'
+
+
 
 def get_last_status():
     with open('.lastst', 'r') as lastst:
@@ -48,6 +55,13 @@ def set_status(event, message, bot, args, config):
     bot.reply(event, "Setze status auf %s. Nicht." % which)
 
 
+def _announce(bot, topic, msg):
+    """Announce a msg of a topic to the subscribed rooms."""
+    for room in list(bot.client.rooms.values()):
+        if room.room_id in SUBS[topic]:
+            bot.send_html(room, msg)
+
+
 def announce_status(message, data, client, bot, config):
     """Schreibt den spacestatus in alle Räume."""
     payload = message.payload.decode('utf8')
@@ -66,84 +80,97 @@ def announce_status(message, data, client, bot, config):
         return
     set_last_status(status)
     shlog.info(status)
+
     msg = '<b>Der Space ist jetzt {}.</b>'.format(status)
-    for room in list(bot.client.rooms.values()):
-        # write to 1:1 chats with me
-        if len(room._members) == 2:
-            bot.send_html(room, msg)
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name in ['spacemaster']:
-            bot.send_html(room,msg)
+    _announce(bot, 'space/status/open', msg)
 
-def announce_klingel(message, data, client, bot, config):
-    """schreibt in einen raum wenn es klingelt"""
-    logging.info("reacting to space/status/klingel")
-    msg = '<b>Klingelingeling!</b>'
+
+
+def announce_generic(message, data, client, bot, config):
+    """schreibt in einen raum wenn eine mqtt empfangen wurde"""
+    logging.info("reacting to {message.topic}")
     shlog.info('klingel')
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name == 'spacemaster':
-            bot.send_html(room,msg)
+    _announce(bot, message.topic, ROOM_MSGS[message.topic])
 
-def announce_count_close(message, data, client, bot, config):
-    """schreibt in einen raum wie oft es geklingelt hat"""
-    payload = message.payload.decode('utf8')
-    logging.info("space/status/klingel/count-CLOSE contained: {}".format(payload))
-    msg = '<b>Es hat {} mal geklingelt, während der Space offen war.</b>'.format(payload)
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name == 'spacemaster':
-            bot.send_html(room,msg)
 
-def announce_count_open(message, data, client, bot, config):
-    """schreibt in einen raum wie oft es geklingelt hat"""
-    payload = message.payload.decode('utf8')
-    logging.info("space/status/klingel/count-OPEN contained: {}".format(payload))
-    msg = '<b>Es hat {} mal geklingelt, während der Space geschlossen war.</b>'.format(payload)
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name == 'spacemaster':
-            bot.send_html(room,msg)
+def subscribe(event, message, bot, args, config):
+    """Abonniert das angegebene MQTT Thema für einen Raum. Es werden nur die space/status/* Themen unterstützt!"""
+    topic = ''
+    if len(args):
+        topic = args[0]
+    if topic not in MSGS:
+        bot.reply(event, "Unbekanntes topic.")
+        return
+    SUBS[topic].append(event['room_id'])
+    bot.reply(event, f"Das Thema {topic} wurde in diesem Raum abonniert.")
+    save_subscriptions()
 
-def announce_door(message, data, client, bot, config):
-    """schreibt in einen Raum den Türstatus"""
-    payload = message.payload.decode('utf8')
-    logging.info("space/status/door contained: {}".format(payload))
-    msg = '<b>Tuerstatus: {}</b>'.format(payload)
-    shlog.info(payload)
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name == 'spacemaster':
-            bot.send_html(room,msg)
+def unsubscribe(event, message, bot, args, config):
+    """Beendet ein Abo für das angegebene Thema für einen Raum."""
+    topic = ''
+    if len(args):
+        topic = args[0]
+    if topic not in MSGS:
+        bot.reply(event, "Unbekanntes topic.")
+        return
+    if event['room_id'] not in SUBS[topic]:
+        bot.reply(event, "Abo für das Thema {topic} in diesem Raum nicht gefunden.")
+        return
+    del SUBS[topic][SUBS[topic].index(event['room_id'])]
+    bot.reply(event, f"Das Abo für das Thema {topic} wurde in diesem Raum beendet.")
+    save_subscriptions()
 
-def announce_closetime(message, data, client, bot, config):
-    """Schreibt in spacemaster wenn eine neue Schliesszeit gesetzt wurde."""
-    payload = message.payload.decode('utf8')
-    logging.info("space/status/closetime contained: {}".format(payload))
-    msg = '<b>Der Space ist bis mindestens {} Uhr offen.</b>'.format(payload)
-    shlog.info('closetime {}'.format(payload))
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name in ('spacemaster', ):
-            bot.send_html(room,msg)
+def list_subscriptions(event, message, bot, args, config):
+    """Zeigt die aktuelle abonnierten Themen in einem Raum an."""
+    topics = []
+    for topic, rooms in SUBS.items():
+        if event['room_id'] in rooms:
+            topics.append(topic)
+    bot.reply(event, f"In diesem Raum sind folgende Themen abonniert: {', '.join(topics)}")
 
-def announce_error(message, data, client, bot, config):
-    """Schreibt Fehlermeldungen in in den Spacemaster Raum."""
-    payload = message.payload.decode('utf8')
-    logging.info("space/status/error contained: {}".format(payload))
-    msg = '<b>FEHLER:</b><br/><i>{}</i>'.format(payload)
-    for room in list(bot.client.rooms.values()):
-        # XXX move to module configuration, allow multiple room names
-        if room.display_name in ('spacemaster'):
-            bot.send_html(room,msg)
+
+def save_subscriptions():
+    """Saves the subscriptions to a file."""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(json.dumps(SUBS).encode())
+        temp_file_path = temp_file.name
+
+    shutil.move(temp_file_path, '.subscriptions')
+
 
 CMDS = {'!status': get_status,
-        '!setstatus': set_status, }
+        '!setstatus': set_status,
+        '!subscribe': subscribe,
+        '!unsubscribe': unsubscribe,
+        '!list_subscriptions': list_subscriptions, }
 
 MSGS = { 'space/status/open': announce_status, 
-         'space/status/klingel/count-OPEN': announce_count_open,
-         'space/status/klingel/count-CLOSE': announce_count_close,
-         'space/status/klingel': announce_klingel,
-         'space/status/closetime': announce_closetime,
-         'space/status/error': announce_error,
-         'space/status/door': announce_door}
+         'space/status/klingel/count-OPEN': announce_generic,
+         'space/status/klingel/count-CLOSE': announce_generic,
+         'space/status/klingel': announce_generic,
+         'space/status/closetime': announce_generic,
+         'space/status/error': announce_generic,
+         'space/status/door': announce_generic}
+
+ROOM_MSGS = {
+         'space/status/klingel/count-OPEN': '<b>Es hat {} mal geklingelt, während der Space geschlossen war.</b>',
+         'space/status/klingel/count-CLOSE': '<b>Es hat {} mal geklingelt, während der Space offen war.</b>',
+         'space/status/klingel': '<b>Klingelingeling!</b><!-- {} -->',
+         'space/status/closetime': '<b>Der Space ist bis mindestens {} Uhr offen.</b>',
+         'space/status/error': '<b>FEHLER:</b><br/><i>{}</i>',
+         'space/status/door': '<b>Tuerstatus: {}</b>'}
+
+
+
+SUBS = { msg: [] for msg in MSGS }
+
+def load_subscriptions():
+    if not os.path.isfile('.subscriptions'):
+        return
+    with open('.subscriptions', 'r') as subfile:
+        subs = json.loads(subfile.read())
+        for topic, rooms in subs.items():
+            if topic in SUBS and isinstance(rooms, list):
+                SUBS[topic] = rooms
+
+load_subscriptions()
